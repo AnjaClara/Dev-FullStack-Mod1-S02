9dafad343f31c9f56cb6c59ef3b6acdecafcbf19
AnjaClara <anaclaraguimaraes1@gmail.com>
2023-03-13 22:28:54 -0300

    EX8

---
+++ ExsSemana2/Ex8/M1S02EX8.js
@@ -0,0 +1,59 @@
+function buttonAdd() {
+  var add = document.getElementById("add");
+  var campos = document.getElementById("campos");
+  var ultimoCampo = campos.lastElementChild;
+  var ultimoValor = parseInt(ultimoCampo.querySelector(".product").value);
+
+  if (ultimoValor === 0) {
+    add.innerHTML = "Você finalizou sua compra! ";
+    return;
+  } else if (isNaN(ultimoValor)){
+    alert ("Digite 0 no último campo para encerrar!");
+  }
+
+  var novoCampo = document.createElement("div");
+  novoCampo.innerHTML = '<label>R$ <input class="product" type="number" placeholder="Produto '+ (campos.children.length + 1) + '"></label>';
+  campos.appendChild(novoCampo);
+
+
+}
+
+function buttonTotal() {
+
+  var totalProduct = document.getElementById('totalProduct');
+  var campos = document.getElementsByClassName("product");
+  var resultado = 0;
+  for (var i = 0; i < campos.length; i++) {
+    var valor = parseInt(campos[i].value);
+    if (!isNaN(valor) && valor !== 0) {
+      resultado += valor;
+    }
+  }
+  totalProduct.innerText = `Total: R$ ${resultado}`;
+}
+
+
+function buttonTroco(){
+
+  var trocoProduct = document.getElementById('trocoProduct');
+  var campos = document.getElementsByClassName("product");
+  var resultado = 0;
+  for (var i = 0; i < campos.length; i++) {
+    var valor = parseInt(campos[i].value);
+    if (!isNaN(valor)) {
+      resultado += valor;
+    }
+  }
+  var subtrair = parseInt(document.getElementById("money").value);
+  if (!isNaN(subtrair)) {
+    var total= subtrair - resultado;
+  } else {
+    alert("Digite um valor válido para subtrair.");
+  }
+  trocoProduct.innerText = `Troco: R$ ${total}`;
+}
+
+var restartButton = document.getElementById("restartButton");
+restartButton.addEventListener("click", function() {
+  location.reload();
+});
